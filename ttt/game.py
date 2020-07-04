337411c144a8b1dd832c3211e029f99c7ff536b7
Dallan Goldblatt <dallangoldblatt@gmail.com>
2020-07-04 13:05:53 -0400

    progress

--- ttt/game.py
+++ ttt/game.py
@@ -1,5 +1,44 @@
 class Game():
 
-    def __init__(self, max_turn):
+    def __init__(self, player_first):
         # An empty space is represented with -1
-        board = [-1] * 9
+        self.board = [-1] * 9
+
+        # If the player lost the coin toss, let the AI move first
+        if not player_first:
+            ai_turn()
+
+
+    def player_turn(self, move):
+        # Set player move
+        self.board[move] = True
+
+        # Check if the game has ended
+        if not GameState(False, self.board, move).ended:
+            ai_turn()
+
+    def ai_turn(self):
+        print('Thinking...')
+
+        # Find best move for AI
+        next_state = GameState(True, self.board, move) # TODO fix move
+        ai_move = next_state.get_max_move()[0]
+
+        # Set AI move
+        self.board[ai_move] = False
+
+        print_board()
+
+    def print_board(self):
+        """
+        0 | 1 | 2
+        ---------
+        3 | 4 | 5
+        ---------
+        6 | 7 | 8
+        """
+        print(f' {self.board[0]} | {self.board[1]} | {self.board[2]}')
+        print(f' ---------')
+        print(f' {self.board[3]} | {self.board[4]} | {self.board[5]}')
+        print(f' ---------')
+        print(f' {self.board[6]} | {self.board[7]} | {self.board[8]}')
--- ttt/gamestate.py
+++ ttt/gamestate.py
@@ -9,14 +9,10 @@ class GameState():
     6 | 7 | 8
     """
 
-
-    def __init__(self, max_turn, board, move):
+    def __init__(self, max_turn, board, last_move):
         self.max_turn = max_turn
-        self.board = board
-        self.move = move
-
-        # Apply move to board
-        self.board[move] = max_turn
+        self.board = board.copy()
+        self.last_move = last_move
 
         # Matches a move to the possible lines that it could complete
         winning_lines =
@@ -31,25 +27,51 @@ class GameState():
             7: [[1, 4, 7], [6, 7, 8]],
             8: [[0, 4, 8] ,[2, 5, 8], [6, 7, 8]]
         }
-        # Check if move ended the game
-        self.win = all([self.board[index] == max_turn for line in winning_lines[self.move] for index in line])
+
+        # Check if last move ended the game
+        self.win = any([self.board[index] == max_turn for line in winning_lines[self.last_move] for index in line]) # TODO any all
         self.tie = not self.win and all([space != -1 for space in self.board])
         self.ended = self.win or self.tie
+
+    def get_max_move(self):
+        # Max seeks to maximize move utility
+        best_move, best_value = -1, -2
+        for index, space in enumerate(self.board):
+            if space != -1:
+                continue
+            next_state = GameState(not max_turn, self.board, index)
+            next_value = next_state.get_utility()
+            if next_value > best_value:
+                best_move = index
+                best_value = next_value
+        return (best_move, best_value)
+
+    def get_min_move(self):
+        # Min seeks to maximize move utility
+        best_move, best_value = -1, 2
+        for index, space in enumerate(self.board):
+            if space != -1:
+                continue
+            next_state = GameState(not max_turn, self.board, index)
+            next_value = next_state.get_utility()
+            if next_value < best_value:
+                best_move = index
+                best_value = next_value
+        return (best_move, best_value)
 
     def get_utility(self):
         if not self.ended:
             # Consider all possible next gamestates
-            for index, space in enumerate(board):
-                if space != -1:
-                    continue
-                # TODO
-
+            if max_turn:
+                return get_max_move()[1]
+            else:
+                return get_min_move()[1]
         elif self.tie:
-            # Neither player wins:
+            # Neither player wins
             return 0
-        elif self.win and max_turn:
-            # Max player wins
+        elif max_turn:
+            # AI wins
             return 1
         else:
-            # Min player wins
+            # Player wins
             return -1
